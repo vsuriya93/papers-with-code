f88ef9edd42406ea7245003fde4ff74c3d6ec380
Suriya venkatesan <suriya.venkatesan@grabtaxi.com>
2021-05-17 21:31:10 +0800

    add gcn code and models

--- Graphs/gcn/model.py
+++ Graphs/gcn/model.py
@@ -1,13 +1,47 @@
+import math
 import torch
 import torch.nn as nn
+from torch.nn.modules import dropout
+import torch.nn.functional as F
+
+class GraphConv(nn.Module):
+    def __init__(self,in_features, out_features,bias = False):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = nn.Parameter(torch.FloatTensor(in_features,out_features))
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(out_features))
+        else:
+            self.register_parameter('bias', None)
+        self._reset_params()
+    
+    def _reset_params(self):
+        stdv = 1. / math.sqrt(self.weight.size(1))
+        # Inplace
+        self.weight.data.uniform_(-stdv, stdv)
+        if self.bias:
+            self.bias.data.uniform_(-stdv, stdv)
+    
+    def forward(self, input, adj):
+        support = input @ self.weight
+        output = adj @ support
+
+        if self.bias:
+            return output + self.bias
+        
+        return output
 
 
 class GCN(nn.Module):
-    def __init__(self):
+    def __init__(self,infeat, nhid, nclass,dropout = .50):
         super().__init__()
-        pass
-        self.layer_1 = nn.Linear(5,6)
+        self.gc1 = GraphConv(infeat, nhid)
+        self.gc2 = GraphConv(nhid, nclass)
+        self.dropout = dropout
 
-    def forward(self,x):
-        print(x.shape)
-        return x
+    def forward(self,input, adj):
+        x = F.relu(self.gc1(input, adj))
+        x = F.dropout(x, self.dropout,training=self.training)
+        x = self.gc2(x, adj)
+        return F.log_softmax(x,dim=1)
